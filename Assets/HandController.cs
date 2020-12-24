c0d0d114dfd8f325ea8ec245530f7530da5914c9
Julie <52919846+MrTetris-rv@users.noreply.github.com>
2020-12-24 10:58:38 +0300

    update

---
+++ Assets/HandController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandController : MonoBehaviour
+{
+    private InputDevice targetDevice;
+    private void Start()
+    {
+
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    private void Update()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+        Debug.Log("Trigger pressed " + triggerValue);
+    }
+
+
+
+}
